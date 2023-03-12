7f826c329754baa1e60b3245d3bcf66b48d5728c
solo015 <123257428+solo015@users.noreply.github.com>
2023-03-12 12:10:18 +0500

    Updated Geterseter.java

--- GetterSetter.java
+++ GetterSetter.java
@@ -4,13 +4,17 @@ public class GetterSetter {
     
    
     public static void main(String cap[]){
+        
         Encapsulation Encap=new Encapsulation();
         Scanner s=new Scanner(System.in);
         System.out.println("Enter Id and Name");
         int    id=s.nextInt();
         String name=s.next();
-        Encap.showDetails(id,name);
-        Encap.getDetails();
+        
+         Encap.setId(id);
+         Encap.setName(name);
+        System.out.println("\n   Id : "+Encap.getId());
+        System.out.println("\n  Name : "+Encap.getName());
       
     
 
@@ -19,14 +23,18 @@ public class GetterSetter {
 class Encapsulation{
     private String name;
     private int id;
-    public void getDetails(){
-        
-        System.out.println("\n  Id : "+this.id);
-         System.out.println("Name : "+this.name);
+    public int setId(){
+        return this.id;
     }
- 
-    public void showDetails(int id,String name){
+  public String setId(){
+        return this.name;
+    }
+    public void getId(int id){
         this.id=id;
+       
+    }
+     public void getName(String name){
+        
         this.name=name;
     }
    
