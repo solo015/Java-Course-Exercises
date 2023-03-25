71c2d954d60e382af604e7d2e0f0aa246e782b41
solo015 <laptopwindow15@gmail.com>
2023-03-25 12:26:24 +0500

    Assignment#2

---
+++ Distance.java
@@ -0,0 +1,6 @@
+/*Task # 6
+Create a distance class. The member data should comprise an int for storing the distance provided, a 
+string for choice of the measurement unit and another int for conversion. Member functions should 
+allow the user to enter the distance in either of the three units (inches, millimeters, kilometers) and 
+convert to the choice of the user and display it
+*/
